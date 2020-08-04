00a852c363ba3f080614f92535f0dd666545af64
keggo97 <eherrera0597@hotmail.com>
2020-08-04 12:16:11 -0500

    validacion login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import {ApiServices} from '../../services/api.services';
-import {UsuarioModel} from '../../models/usuario.model';
-import {NgForm} from '@angular/forms';
-import {Router} from '@angular/router';
+import { ApiServices } from '../../services/api.services';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
-import {AuthApiServices} from '../../services/auth.services';
-import {ChatService} from '../../services/chat.services';
+import { AuthApiServices } from '../../services/auth.services';
+import { ChatService } from '../../services/chat.services';
 
 @Component({
   selector: 'app-iniciarsesion',
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit {
 
   usuario: UsuarioModel = new UsuarioModel();
 
-  constructor( private apiservice: AuthApiServices, private router: Router, private apichatservices: ChatService) {
+  constructor(private apiservice: AuthApiServices, private router: Router, private apichatservices: ChatService) {
     this.intro = document.getElementById('body');
     this.intro.classList.add('body-login');
     this.wrapper = document.getElementById('content-wrapper-body');
@@ -29,24 +29,45 @@ export class LoginComponent implements OnInit {
 
   login(form: NgForm) {
     console.log(form);
-    this.usuario.email = form.value.email;
-    this.usuario.password = form.value.password;
-    return this.apiservice.login(this.usuario).subscribe( (resp: any) => {
-      console.log(resp);
-      if (resp.message === 'ok') {
-        this.apichatservices.setupSocketConnection();
-        this.apichatservices.joinRoom(resp.data.allData[0]['id']);
-        this.router.navigateByUrl('dashboard');
+    var undefin="";
+    var usu = form.value.email;
+    var pass = form.value.password;
+    console.log(usu, pass)
+    if (usu == null || pass == null) {
+      if (usu == null) {
+        undefin="Ingrese el Usuario"
       }
-    }, error => {
-      console.log(error);
+      else {
+        undefin="Ingrese la Contraseña"
+      }
       Swal.fire({
         icon: 'error',
-        title: error.error.message,
+        title: undefin,
         showConfirmButton: false,
         timer: 1500
+      })
+
+    }
+    else {
+      this.usuario.email = form.value.email;
+      this.usuario.password = form.value.password;
+      return this.apiservice.login(this.usuario).subscribe((resp: any) => {
+        console.log(resp);
+        if (resp.message === 'ok') {
+          this.apichatservices.setupSocketConnection();
+          this.apichatservices.joinRoom(resp.data.allData[0]['id']);
+          this.router.navigateByUrl('dashboard');
+        }
+      }, error => {
+        console.log(error);
+        Swal.fire({
+          icon: 'error',
+          title: error.error.message,
+          showConfirmButton: false,
+          timer: 1500
+        });
       });
-    });
+    }
   }
 
 }
